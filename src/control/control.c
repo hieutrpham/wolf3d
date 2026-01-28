c0dda9fc3be928a774198399b91f7cec9b5bd6b5
hieutrpham <hieutrungpham91@gmail.com>
2026-01-28 14:36:09 +0200

    feat: add Left, Right arrows, A, D to move player

--- src/control/control.c
+++ src/control/control.c
@@ -18,7 +18,7 @@ void	key_control(mlx_key_data_t keydata, void *param)
 
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(game->mlx);
-	if (keydata.key == MLX_KEY_A)
+	if (keydata.key == MLX_KEY_LEFT)
 	{
 		game->player->angle -= 0.1f;
 		if (game->player->angle < 0)
@@ -26,7 +26,7 @@ void	key_control(mlx_key_data_t keydata, void *param)
 		game->player->dx = cosf(game->player->angle) * 10;
 		game->player->dy = sinf(game->player->angle) * 10;
 	}
-	if (keydata.key == MLX_KEY_D)
+	if (keydata.key == MLX_KEY_RIGHT)
 	{
 		game->player->angle += 0.1f;
 		if (game->player->angle < 0)
@@ -34,6 +34,10 @@ void	key_control(mlx_key_data_t keydata, void *param)
 		game->player->dx = cosf(game->player->angle) * 10;
 		game->player->dy = sinf(game->player->angle) * 10;
 	}
+	if (keydata.key == MLX_KEY_A)
+		game->player->pos.x = game->player->pos.x - (int)game->player->dx;
+	if (keydata.key == MLX_KEY_D)
+		game->player->pos.x = game->player->pos.x + (int)game->player->dx;
 	if (keydata.key == MLX_KEY_W)
 	{
 		game->player->pos.x = game->player->pos.x + (int)game->player->dx;
